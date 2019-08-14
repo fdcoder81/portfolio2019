4eaf55843e29c0b51f121716c7a4194785e5ffd3
Fdcoder81 <carloanselmi81@gmail.com>
2019-08-14 12:16:14 +0200

    Updates

---
+++ precache-manifest.09f95a39e230f6261f10760b416e749b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6b5a9624dcfcf619cb8a6e236b438b",
+    "url": "/portfolio2019/index.html"
+  },
+  {
+    "revision": "5e7b2091b50e949e2ecc",
+    "url": "/portfolio2019/static/css/main.a07d6aca.chunk.css"
+  },
+  {
+    "revision": "91be5a23204fcbd1ed3e",
+    "url": "/portfolio2019/static/js/2.35f64330.chunk.js"
+  },
+  {
+    "revision": "5e7b2091b50e949e2ecc",
+    "url": "/portfolio2019/static/js/main.f352a67f.chunk.js"
+  },
+  {
+    "revision": "1b8f21f30d9826a2fafe",
+    "url": "/portfolio2019/static/js/runtime~main.8abb7632.js"
+  },
+  {
+    "revision": "bee36bb775304128ae2e20fcb30e4772",
+    "url": "/portfolio2019/static/media/background.bee36bb7.jpg"
+  },
+  {
+    "revision": "072102c30135a40aa11f56aef8a17dc9",
+    "url": "/portfolio2019/static/media/bio.072102c3.jpg"
+  },
+  {
+    "revision": "4a06041dbce6f5d58b6ee38af202bad1",
+    "url": "/portfolio2019/static/media/movies-x.4a06041d.png"
+  },
+  {
+    "revision": "9547d3f9249410354a27491bbb5f1d5d",
+    "url": "/portfolio2019/static/media/portfolio.9547d3f9.png"
+  },
+  {
+    "revision": "dccf56f611289827f368c7057189b37e",
+    "url": "/portfolio2019/static/media/trivia.dccf56f6.png"
+  },
+  {
+    "revision": "bf021aa9e0ee80da1bd74c56ee4d1b9a",
+    "url": "/portfolio2019/static/media/udemy.bf021aa9.png"
+  },
+  {
+    "revision": "79f822b83f4c6ae40453d4e8e386889a",
+    "url": "/portfolio2019/static/media/weather-app.79f822b8.png"
+  }
+]);
